798d9500dab0b16bc513af7f4ae53bcabbb70ed3
Philip Schneider <philip,schneider@sulzer.de>
2019-09-18 14:15:20 +0200

    Emojis for 2020

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import json
 import xml.etree.ElementTree as ElementTree
 
-x = open("emoji4j.json").read()
-j = json.loads(x)
-
-root = ElementTree.parse("en.xml").getroot()
-
+# Parse XML
 x = []
 lastemoji = ""
 e = {}
+root = ElementTree.parse("en.xml").getroot()
 for annotation in root.findall('annotations/annotation'):
     emoji = annotation.get("cp")
     if lastemoji != emoji:
@@ -17,11 +14,42 @@ for annotation in root.findall('annotations/annotation'):
             x.append(e)
         e = {"emoji": emoji}
     if annotation.get("type") == "tts":
-        e["description"] = annotation.text
+        e["description"] = annotation.text.split()
     else:
-        e["tags"] = annotation.text
+        tags = annotation.text.split("|")
+        e["tags"] = [i.strip() for i in tags]
 x.append(e)
 
-f = open("emoji.json", "w")
+# Parse JSON
+y = open("emoji4j.json", encoding="utf8").read()
+j = json.loads(y)
+for e in j:
+    for o in x:
+        if o["emoji"] == e["emoji"]:
+            o["aliases"] = e["aliases"]
+            if "emoticons" in e:
+                o["emoticons"] = e["emoticons"]
+
+# Parse TXT
+fileHandler = open("groups.txt", "r", encoding="utf8")
+listOfLines = fileHandler.readlines()
+fileHandler.close()
+for line in listOfLines:
+    if line[0:8] == "# group:":
+        group = line[9:].strip()
+        print(group)
+    if line[0:11] == "# subgroup:":
+        subgroup = line[12:].strip()
+        print("--" + subgroup)
+    if len(line) > 70:
+        if line[65] == "#":
+            emoji = line[67:69].strip()
+            print(emoji)
+            for o in x:
+                if o["emoji"] == emoji:
+                    o["group"] = group
+                    o["subgroup"] = subgroup
+
+f = open("emoji.json", "w", encoding="utf8")
 f.write(json.dumps(x))
 f.close()
